de66602f6bea9c76d2dd32c0fe6b60b7c3a8beee
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2023-01-04 07:45:53 +0900

    ✨ 楽曲カードのハイパーリンクをコンポーネント全体にかかるように変更

--- src/components/MusicCard.svelte
+++ src/components/MusicCard.svelte
@@ -7,21 +7,22 @@
 </script>
 
 <div class="card card-compact w-64 bg-base-100 shadow-xl justify-center">
-  <figure><img src={imgSrc} alt={title} /></figure>
-  <div class="card-body">
-    <a href={aHref}>
+  <a href={aHref}>
+    <figure><img src={imgSrc} alt={title} /></figure>
+    <div class="card-body">
       <h2 class="card-title">
         {title}
       </h2>
-    </a>
-    <p />
-    <div class="card-actions justify-end">
-      <div class="tooltip" data-tip="作曲・編曲">
-        <div class="badge badge-primary">{artist}</div>
-      </div>
-      <div class="tooltip" data-tip="譜面作成">
-        <div class="badge badge-secondary">{noter}</div>
+
+      <p />
+      <div class="card-actions justify-end">
+        <div class="tooltip" data-tip="作曲・編曲">
+          <div class="badge badge-primary">{artist}</div>
+        </div>
+        <div class="tooltip" data-tip="譜面作成">
+          <div class="badge badge-secondary">{noter}</div>
+        </div>
       </div>
     </div>
-  </div>
+  </a>
 </div>
