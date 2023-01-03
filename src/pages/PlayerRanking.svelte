69b007814c485383f1f6bef727d9bf3511040f18
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2023-01-04 02:32:06 +0900

    ✨ 総合ランキングを実装

---
+++ src/pages/PlayerRanking.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { getNameColor } from "../lib/nameColor"
+  let i = 0
+
+  const fetchUsers = (async () => {
+    const response = await fetch(`https://ricora-beats-ir.fly.dev/users/`)
+    const ranking = await response.json()
+    ranking.sort((a, b) => b.performance_point - a.performance_point)
+    return ranking
+  })()
+</script>
+
+<main>
+  <div class="divider"><h2 class="text-xl font-bold">総合ランキング</h2></div>
+
+  {#await fetchUsers}
+    <div class="flex p-8 justify-center">
+      <div
+        class="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"
+      />
+    </div>
+  {:then users}
+    <div class="p-6 overflow-x-auto w-full">
+      <table class="table w-full">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Performance</th>
+            <th />
+          </tr>
+        </thead>
+
+        <tbody>
+          {#each users as user, i}
+            <tr class="hover">
+              <td>{i + 1}</td>
+              <td>
+                <div class="flex items-center space-x-3">
+                  <div>
+                    <div
+                      class="font-bold"
+                      style="color: {getNameColor(user.performance_point)};"
+                    >
+                      {user.screen_name}
+                    </div>
+                    <div class="text-sm opacity-50">ID: {user.id}</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <div class="font-bold">{user.performance_point}</div>
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+  {/await}
+</main>
+
+<style>
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import Home from "../pages/Home.svelte"
 import MusicList from "../pages/MusicList.svelte"
 import MusicDetail from "../pages/MusicDetail.svelte"
+import PlayerRanking from "../pages/PlayerRanking.svelte"
 
 export const routes = {
   "/": Home,
   "/music": MusicList,
   "/music/:folder": MusicDetail,
+  "/ranking": PlayerRanking,
   "*": Home,
 }
