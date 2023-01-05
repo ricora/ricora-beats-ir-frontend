9f66711a4a395aa6b4d0b218070779b6947f25d5
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2023-01-06 01:24:31 +0900

    ✨ ユーザーページで自己紹介を表示する

--- src/pages/UserDetail.svelte
+++ src/pages/UserDetail.svelte
@@ -98,9 +98,35 @@
                   <span class="badge badge-md"
                     >{user.performance_point} pts.</span
                   >
-                </span></span
-              >
+                </span>
+              </span>
             </div>
+            {#if user.self_introduction}
+              <div class="alert max-w-4xl shadow-lg">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    class="stroke-info flex-shrink-0 w-6 h-6"
+                    ><path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    /></svg
+                  >
+                  <div>
+                    <h3 class="font-bold">自己紹介</h3>
+                    <div>
+                      <p>
+                        {user.self_introduction}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            {/if}
           </div>
         </div>
       {:catch error}
