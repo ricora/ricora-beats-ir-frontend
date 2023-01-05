b073ec5f3bc681a759d43b8d7e0bc262c48a99c5
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2023-01-06 00:25:00 +0900

    ✨ ゲームの説明を追加

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -29,9 +29,181 @@
           href="https://github.com/RICORA/ricora-beats/wiki"
           target="_blank"
           rel="noopener noreferrer"
-          ><button class="btn btn-neutral">RICORA Beatsとは？</button></a
+          ><button class="btn btn-neutral">RICORA Beats Wiki</button></a
         >
       </div>
     </div>
   </div>
 </div>
+
+<div class="hero min-h-30 bg-1">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="flex-row">
+      <div class="py-2">
+        <img
+          src="https://user-images.githubusercontent.com/52315048/210119143-7ece6a92-8cd1-4b67-8460-744f099a6e54.png"
+          class="sm:max-w-md rounded-lg shadow-2xl"
+        />
+      </div>
+      <div class="py-2">
+        <img
+          src="https://user-images.githubusercontent.com/52315048/210119985-7a97cdd7-e0c7-47eb-a664-34d072292452.png"
+          class="sm:max-w-md rounded-lg shadow-2xl"
+        />
+      </div>
+    </div>
+    <div>
+      <h2 class="text-5xl font-bold">RICORA Beatsとは？</h2>
+      <p class="py-6">
+        <a
+          href="https://github.com/RICORA/ricora-beats/"
+          class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
+          >RICORA Beats</a
+        >は<a
+          href="https://tus-ricora.com/"
+          class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
+          rel="nofollow">東京理科大学 電子計算機研究会RICORA</a
+        >の有志部員によって制作された音楽(リズム)ゲームです。より多くの人にプレーしてほしいという思想の元、導入の敷居が低い<a
+          href="https://developer.mozilla.org/ja/docs/Web/Progressive_web_apps"
+          class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
+          rel="nofollow">PWA</a
+        >対応のWebアプリとして開発され、タッチ操作とキーボード操作のどちらでも遊べるマルチプラットフォームアプリとして設計されています。楽曲を<a
+          href="https://music.tus-ricora.com/"
+          class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
+          rel="nofollow">音楽班</a
+        >が、プログラムを<a
+          href="https://alg.tus-ricora.com/"
+          class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
+          rel="nofollow">プログラミング班</a
+        >が主に担当しています。
+      </p>
+      <a
+        href="https://github.com/RICORA/ricora-beats"
+        target="_blank"
+        rel="noopener noreferrer"
+        ><button class="btn btn-neutral">View on GitHub</button></a
+      >
+    </div>
+  </div>
+</div>
+
+<div class="hero min-h-30 bg-2">
+  <div class="hero-content flex-col lg:flex-row">
+    <img
+      src="https://user-images.githubusercontent.com/52315048/210120164-84ad19b2-6bda-4a44-a111-e4c762b6a7e3.png"
+      class="sm:max-w-md rounded-lg shadow-2xl"
+    />
+    <div>
+      <h2 class="text-5xl font-bold">幅広い難易度</h2>
+      <p class="py-6">難易度は4段階、レーン数は4つから選ぶことができます。</p>
+      <table>
+        <tbody>
+          <div class="py-2">
+            <tr>
+              <td>
+                <img
+                  src="./assets/img/diff_icon_1.png"
+                  class="px-1"
+                  alt="BEGINNER"
+                /></td
+              >
+              <td>
+                <img
+                  src="./assets/img/diff_icon_2.png"
+                  class="px-1"
+                  alt="STANDARD"
+                /></td
+              >
+              <td>
+                <img
+                  src="./assets/img/diff_icon_3.png"
+                  class="px-1"
+                  alt="ADVANCED"
+                /></td
+              >
+              <td>
+                <img
+                  src="./assets/img/diff_icon_4.png"
+                  class="px-1"
+                  alt="EXTRA"
+                /></td
+              >
+            </tr>
+          </div>
+        </tbody>
+      </table>
+      <table>
+        <tbody>
+          <div class="py-2">
+            <tr>
+              <td>
+                <img
+                  src="./assets/img/key_icon_4.png"
+                  class="px-1"
+                  alt="4 KEYS"
+                /></td
+              >
+              <td>
+                <img
+                  src="./assets/img/key_icon_5.png"
+                  class="px-1"
+                  alt="5 KEYS"
+                /></td
+              >
+              <td>
+                <img
+                  src="./assets/img/key_icon_6.png"
+                  class="px-1"
+                  alt="6 KEYS"
+                /></td
+              >
+              <td>
+                <img
+                  src="./assets/img/key_icon_7.png"
+                  class="px-1"
+                  alt="7 KEYS"
+                /></td
+              >
+            </tr>
+          </div>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+<div class="hero min-h-30 bg-3">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img
+      src="https://user-images.githubusercontent.com/52315048/210120049-f98f929a-8ec8-48f9-831d-2794225f568c.png"
+      class="sm:max-w-md rounded-lg shadow-2xl"
+    />
+    <div>
+      <h2 class="text-5xl font-bold">インターネットランキング</h2>
+      <p class="py-6">
+        アカウントを作成しスコアを送信することで、世界中のプレーヤーと腕前を競うことができます。送信したベストスコアは楽曲別、総合ランキングやマイページで確認できます。
+      </p>
+      <a href="#/music">
+        <button class="btn btn-primary">楽曲別ランキングを見る</button></a
+      >
+      <a href="#/ranking"
+        ><button class="btn btn-secondary">総合ランキングを見る</button></a
+      >
+    </div>
+  </div>
+</div>
+
+<style>
+  button {
+    text-transform: none;
+  }
+  .bg-1 {
+    background-color: #fde6e9;
+  }
+  .bg-2 {
+    background-color: #e6f2fe;
+  }
+  .bg-3 {
+    background-color: #fdefda;
+  }
+</style>
