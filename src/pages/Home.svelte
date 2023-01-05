745659edec30f22842b9d2b42356671d1d5f65be
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2023-01-06 06:37:52 +0900

    🎨 トップページのデザインを修正

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -53,35 +53,59 @@
       </div>
     </div>
     <div>
-      <h2 class="text-5xl font-bold">RICORA Beatsとは？</h2>
-      <p class="py-6">
+      <h2 class="text-5xl font-bold text-1">RICORA Beatsとは？</h2>
+      <p class="py-6 text-1">
         <a
           href="https://github.com/RICORA/ricora-beats/"
+          target="_blank"
+          rel="noopener noreferrer"
           class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
           >RICORA Beats</a
         >は<a
           href="https://tus-ricora.com/"
+          target="_blank"
+          rel="noopener noreferrer"
           class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
-          rel="nofollow">東京理科大学 電子計算機研究会RICORA</a
+          >東京理科大学 電子計算機研究会RICORA</a
         >の有志部員によって制作された音楽(リズム)ゲームです。より多くの人にプレーしてほしいという思想の元、導入の敷居が低い<a
           href="https://developer.mozilla.org/ja/docs/Web/Progressive_web_apps"
+          target="_blank"
+          rel="noopener noreferrer"
           class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
-          rel="nofollow">PWA</a
+          >PWA</a
         >対応のWebアプリとして開発され、タッチ操作とキーボード操作のどちらでも遊べるマルチプラットフォームアプリとして設計されています。楽曲を<a
           href="https://music.tus-ricora.com/"
+          target="_blank"
+          rel="noopener noreferrer"
           class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
-          rel="nofollow">音楽班</a
+          >音楽班</a
         >が、プログラムを<a
           href="https://alg.tus-ricora.com/"
+          target="_blank"
+          rel="noopener noreferrer"
           class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
-          rel="nofollow">プログラミング班</a
+          >プログラミング班</a
         >が主に担当しています。
       </p>
       <a
         href="https://github.com/RICORA/ricora-beats"
         target="_blank"
         rel="noopener noreferrer"
-        ><button class="btn btn-neutral">View on GitHub</button></a
+        ><button class="btn btn-1"
+          ><div class="grid grid-flow-col gap-2 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 23"
+              class="fill-current"
+            >
+              <path
+                d="M 11.964844 0 C 5.347656 0 0 5.269531 0 11.792969 C 0 17.003906 3.425781 21.417969 8.179688 22.976562 C 8.773438 23.09375 8.992188 22.722656 8.992188 22.410156 C 8.992188 22.136719 8.972656 21.203125 8.972656 20.226562 C 5.644531 20.929688 4.953125 18.820312 4.953125 18.820312 C 4.417969 17.453125 3.625 17.101562 3.625 17.101562 C 2.535156 16.378906 3.703125 16.378906 3.703125 16.378906 C 4.914062 16.457031 5.546875 17.589844 5.546875 17.589844 C 6.617188 19.386719 8.339844 18.878906 9.03125 18.566406 C 9.132812 17.804688 9.449219 17.277344 9.785156 16.984375 C 7.132812 16.710938 4.339844 15.695312 4.339844 11.167969 C 4.339844 9.878906 4.8125 8.824219 5.566406 8.003906 C 5.445312 7.710938 5.03125 6.5 5.683594 4.878906 C 5.683594 4.878906 6.695312 4.566406 8.972656 6.089844 C 9.949219 5.832031 10.953125 5.703125 11.964844 5.699219 C 12.972656 5.699219 14.003906 5.835938 14.957031 6.089844 C 17.234375 4.566406 18.242188 4.878906 18.242188 4.878906 C 18.898438 6.5 18.480469 7.710938 18.363281 8.003906 C 19.136719 8.824219 19.589844 9.878906 19.589844 11.167969 C 19.589844 15.695312 16.796875 16.691406 14.125 16.984375 C 14.558594 17.355469 14.933594 18.058594 14.933594 19.171875 C 14.933594 20.753906 14.914062 22.019531 14.914062 22.410156 C 14.914062 22.722656 15.132812 23.09375 15.726562 22.976562 C 20.480469 21.414062 23.910156 17.003906 23.910156 11.792969 C 23.929688 5.269531 18.558594 0 11.964844 0 Z M 11.964844 0 "
+              />
+            </svg>View on GitHub
+          </div></button
+        ></a
       >
     </div>
   </div>
@@ -94,8 +118,10 @@
       class="sm:max-w-md rounded-lg shadow-2xl"
     />
     <div>
-      <h2 class="text-5xl font-bold">幅広い難易度</h2>
-      <p class="py-6">難易度は4段階、レーン数は4つから選ぶことができます。</p>
+      <h2 class="text-5xl font-bold text-2">幅広い難易度と充実した譜面</h2>
+      <p class="py-6 text-2">
+        難易度は4段階、レーン数は4-7の4つから選ぶことができます。
+      </p>
       <table>
         <tbody>
           <div class="py-2">
@@ -179,16 +205,10 @@
       class="sm:max-w-md rounded-lg shadow-2xl"
     />
     <div>
-      <h2 class="text-5xl font-bold">インターネットランキング</h2>
-      <p class="py-6">
+      <h2 class="text-5xl font-bold text-3">インターネットランキング</h2>
+      <p class="py-6 text-3">
         アカウントを作成しスコアを送信することで、世界中のプレーヤーと腕前を競うことができます。送信したベストスコアは楽曲別、総合ランキングやマイページで確認できます。
       </p>
-      <a href="#/music">
-        <button class="btn btn-primary">楽曲別ランキングを見る</button></a
-      >
-      <a href="#/ranking"
-        ><button class="btn btn-secondary">総合ランキングを見る</button></a
-      >
     </div>
   </div>
 </div>
@@ -196,6 +216,24 @@
 <style>
   button {
     text-transform: none;
+  }
+
+  .text-1 {
+    color: #63343a;
+  }
+  .text-2 {
+    color: #344c64;
+  }
+  .text-3 {
+    color: #63461c;
+  }
+  .btn-1 {
+    background-color: #c76874;
+    border-color: #c76874;
+  }
+  .btn-3 {
+    background-color: #c78d39;
+    border-color: #c78d39;
   }
   .bg-1 {
     background-color: #fde6e9;
