b9a37e96cafc10593081b5bfb4affc00c4f64257
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2023-02-11 15:55:52 +0900

    🎨 外部リンクにアイコンを追加

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -22,14 +22,47 @@
           href="https://beats.tus-ricora.com"
           target="_blank"
           rel="noopener noreferrer"
-          ><button class="btn btn-neutral">RICORA Beatsのプレーはこちら</button
+          ><button class="btn btn-neutral"
+            ><div class="grid grid-flow-col gap-2 items-center">
+              RICORA Beatsのプレーはこちら<svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                />
+              </svg>
+            </div></button
           ></a
         >
         <a
           href="https://github.com/RICORA/ricora-beats/wiki"
           target="_blank"
           rel="noopener noreferrer"
-          ><button class="btn btn-neutral">RICORA Beats Wiki</button></a
+          ><button class="btn btn-neutral"
+            ><div class="grid grid-flow-col gap-2 items-center">
+              RICORA Beats Wiki<svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                />
+              </svg>
+            </div></button
+          ></a
         >
       </div>
     </div>
